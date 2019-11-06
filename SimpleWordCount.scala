0c2f7d7dd286e613557b852f61e185707c9a5257
Shudip Datta <sdnv5@mst.edu>
2019-11-06 12:56:35 -0800

    Update and rename WordCount.scala to SimpleWordCount.scala

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -6,4 +6,4 @@ val sorted = wordCount.sortBy(-_._2)
 val filter = sorted.filter(_._2 > 1)
 filter.saveAsTextFile("OutputFolder")
 
-//spark-shell -i '/home/cloudera/Downloads/Spark_Demo-master/WordCount.scala'
+//spark-shell -i '/home/cloudera/Downloads/Spark_Demo-master/SimpleWordCount.scala'
