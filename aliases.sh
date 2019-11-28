f0d7980aa9ca7a3e6fe7934eb6f166b764d65f64
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 15:27:44 -0600

    Trying abs path for alias too

--- aliases.sh
+++ aliases.sh
@@ -5,5 +5,5 @@ function x      { git update-index --chmod=+x $1 ; }
 function reload { git fetch ; git rebase origin/master ; source aliases.sh ; }
 
 
-function spark  { spark-shell -i ./SimpleWordCount.scala ; }
+function spark  { spark-shell -i /home/maria_dev/Spark_Demo/SimpleWordCount.scala ; }
 
