91fd5924f52ef3c39a0bec3e96a23dc3aacf7443
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 14:36:49 -0600

    Using abs path

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -1,11 +1,11 @@
 
 
-val loadfile = sc.textFile("input/input1.txt")
+val loadfile = sc.textFile("/home/maria_dev/Spark_Demo/input/input1.txt")
 val words = loadfile.flatMap(line => line.split(" "))
 val wordMap = words.map(word => (word,1))
 val wordCount = wordMap.reduceByKey(_+_)
 val sorted = wordCount.sortBy(-_._2)
 val filter = sorted.filter(_._2 > 1)
-filter.saveAsTextFile("OutputFolder")
+filter.saveAsTextFile("/home/maria_dev/Spark_Demo/output.txt")
 System.exit(0)
 
