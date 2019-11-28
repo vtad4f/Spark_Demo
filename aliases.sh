576d45562642b83fc3e92013fee714955b67bdbe
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 12:22:08 -0600

    Adding aliases file

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function clean  { git clean -dfqX ; }
+function x      { git update-index --chmod=+x $1 ; }
+function reload { git fetch ; git rebase origin/master ; source aliases.sh ; }
+
+
+function spark  { spark-shell -i ./SimpleWordCount.scala ; }
+
