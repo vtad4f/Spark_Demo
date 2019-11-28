a668aa430765d976c0b20bfec8a3461717c226e8
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 14:21:49 -0600

    Moving input into folder

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -1,6 +1,6 @@
 
 
-val loadfile = sc.textFile("InputFolder/input1.txt")
+val loadfile = sc.textFile("input/input1.txt")
 val words = loadfile.flatMap(line => line.split(" "))
 val wordMap = words.map(word => (word,1))
 val wordCount = wordMap.reduceByKey(_+_)
