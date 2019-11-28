ea9130c88822f57e3a2390994ecb9a240651a00e
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 16:39:03 -0600

    Using the hadoop fs commands

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -1,6 +1,6 @@
 
 
-val loadfile = sc.textFile("/home/maria_dev/Spark_Demo/input/input1.txt")
+val loadfile = sc.textFile("InputFolder/input1.txt")
 val words = loadfile.flatMap(line => line.split(" "))
 val wordMap = words.map(word => (word,1))
 val wordCount = wordMap.reduceByKey(_+_)
--- aliases.sh
+++ aliases.sh
@@ -5,5 +5,18 @@ function x      { git update-index --chmod=+x $1 ; }
 function reload { git fetch ; git rebase origin/master ; source aliases.sh ; }
 
 
-function spark  { spark-shell -i /home/maria_dev/Spark_Demo/SimpleWordCount.scala ; }
+SPARK_DIR=/home/cloudera/git/vtad4f/Spark_Demo
+
+function spark
+{
+   hadoop fs -rm -r InputFolder
+   hadoop fs -mkdir InputFolder
+   hadoop fs -copyFromLocal "$SPARK_DIR/input/input1.txt"
+   
+   spark-shell -i $SPARK_DIR/SimpleWordCount.scala
+   
+   hadoop fs -rm -r OutputFolder
+   hadoop fs -ls OutputFolder
+   hadoop fs -cat OutputFolder/part-00000
+}
 
