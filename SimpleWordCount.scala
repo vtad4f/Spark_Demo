2f339ee43c78d1b45f52c1bc27e9851801aa2dcc
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 12:19:01 -0600

    Adding sys exit at the end of the script

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -1,3 +1,5 @@
+
+
 val loadfile = sc.textFile("InputFolder/input1.txt")
 val words = loadfile.flatMap(line => line.split(" "))
 val wordMap = words.map(word => (word,1))
@@ -5,5 +7,5 @@ val wordCount = wordMap.reduceByKey(_+_)
 val sorted = wordCount.sortBy(-_._2)
 val filter = sorted.filter(_._2 > 1)
 filter.saveAsTextFile("OutputFolder")
+System.exit(0)
 
-//spark-shell -i '/home/cloudera/Downloads/Spark_Demo-master/SimpleWordCount.scala'
