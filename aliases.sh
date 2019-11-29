884261593b74362eba7d52a1680c62712724838d
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 20:25:41 -0600

    Oops, wasn't copying to InputFolder

--- aliases.sh
+++ aliases.sh
@@ -4,15 +4,16 @@ function clean  { git clean -dfqX ; }
 function x      { git update-index --chmod=+x $1 ; }
 function reload { git fetch ; git rebase origin/master ; source aliases.sh ; }
 
-SPARK_DIR=/home/cloudera/git/vtad4f/Spark_Demo
+INPUT_DIR=InputFolder
+REPO_DIR=/home/cloudera/git/vtad4f/Spark_Demo
 
 function spark
 {
-   hadoop fs -rm -r InputFolder
-   hadoop fs -mkdir InputFolder
-   hadoop fs -copyFromLocal "$SPARK_DIR/input/input1.txt"
+   hadoop fs -rm -r $INPUT_DIR
+   hadoop fs -mkdir $INPUT_DIR
+   hadoop fs -copyFromLocal $REPO_DIR/input/*.txt $INPUT_DIR
    
-   spark-shell -i $SPARK_DIR/SimpleWordCount.scala
+   spark-shell -i $REPO_DIR/SimpleWordCount.scala
    
    hadoop fs -rm -r OutputFolder
    hadoop fs -cat OutputFolder/part-00000
