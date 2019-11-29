709086f1ef000c57984303c0e70d3daabb0e26a5
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-28 22:00:06 -0600

    Trying non-shorthand reducebykey fcn

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -2,10 +2,14 @@
 
 val loadfile = sc.textFile("InputFolder/input1.txt")
 val words = loadfile.flatMap(line => line.split(" "))
+
 val wordMap = words.map(word => (word,1))
-val wordCount = wordMap.reduceByKey(_+_)
-val sorted = wordCount.sortBy(-_._2)
+val wordCount = wordMap.reduceByKey((v1, v2) => v1 + v2)
+
+// val sorted = wordCount.sortBy(-_._2)
 // val filter = sorted.filter(_._2 > 1)
+
 sorted.saveAsTextFile("OutputFolder")
+
 System.exit(0)
 
