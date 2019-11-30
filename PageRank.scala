6cbb6f528f79f97cc8d51e65f1ca18dfe970e559
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-29 20:31:19 -0600

    Trying list buffer

--- PageRank.scala
+++ PageRank.scala
@@ -1,17 +1,21 @@
 
+
+import scala.collection.mutable.ListBuffer
+
+
 val pattern = "[^\\s\"\\[\\]',]+".r
 
 def MyMap(line : String) : List[(String, Array[Double])] = {
    
    val pages = pattern.findAllIn(line).toArray
    
-   val ret : List[(String, Array[Double])] = List()
-   ret = ret :+ (pages(0), Array(1.0 - 0.85, pages.length - 1.0, 0.0))
+   val ret = ListBuffer[(String, Array[Double])]()
+   ret += (pages(0), Array(1.0 - 0.85, pages.length - 1.0, 0.0))
    
    for (i <- 1 to (pages.length - 1)) {
-      ret = ret :+ (pages(i), Array(0.85 * 1.0 / (pages.length - 1.0), 0.0, 1.0))
+      ret += (pages(i), Array(0.85 * 1.0 / (pages.length - 1.0), 0.0, 1.0))
    }
-   return ret
+   return ret.toList
 }
 
 val lines = sc.textFile("InputFolder/PRData.txt")
