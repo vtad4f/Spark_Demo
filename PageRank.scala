5d6fb94eb610a136abc00d110e0330c45d9a0591
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-11-29 20:34:44 -0600

    Trying an extra set of parentheses like on stack overflow

--- PageRank.scala
+++ PageRank.scala
@@ -10,10 +10,10 @@ def MyMap(line : String) : List[(String, Array[Double])] = {
    val pages = pattern.findAllIn(line).toArray
    
    val ret = ListBuffer[(String, Array[Double])]()
-   ret += (pages(0), Array(1.0 - 0.85, pages.length - 1.0, 0.0))
+   ret += ((pages(0), Array(1.0 - 0.85, pages.length - 1.0, 0.0)))
    
    for (i <- 1 to (pages.length - 1)) {
-      ret += (pages(i), Array(0.85 * 1.0 / (pages.length - 1.0), 0.0, 1.0))
+      ret += ((pages(i), Array(0.85 * 1.0 / (pages.length - 1.0), 0.0, 1.0)))
    }
    return ret.toList
 }
